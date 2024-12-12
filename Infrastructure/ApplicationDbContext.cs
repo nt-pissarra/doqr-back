05fa6da5b435a8138b6f8b3b6794ae3d8535fb18
nt-pissarra <ntpissarra.dev@gmail.com>
2024-12-12 04:28:23 -0300

    feat: create initial migration

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace Infrastructure
 {
@@ -9,5 +10,24 @@ namespace Infrastructure
 
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(ToSnakeCase(entity.GetTableName()));
+
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(ToSnakeCase(property.Name));
+                }
+            }
+        }
+
+        private string ToSnakeCase(string input)
+        {
+            return Regex.Replace(input, @"([a-z])([A-Z])", "$1_$2").ToLower();
+        }
     }
 }
